0b1378bec3497b73a98a5502edbe6941e1bd8471
huang12zheng <805104533@qq.com>
2022-06-01 22:23:01 +0800

    move code from layout_setting to layout_wrap

--- lib/main.data.dart
+++ lib/main.data.dart
@@ -1,5 +1,3 @@
-
-
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: directives_ordering, top_level_function_literal_block
 
@@ -11,15 +9,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flex_demo/widget/layout/provider/layout_setting.dart';
 
 // ignore: prefer_function_declarations_over_variables
-ConfigureRepositoryLocalStorage configureRepositoryLocalStorage = ({FutureFn<String>? baseDirFn, List<int>? encryptionKey, bool? clear}) {
+ConfigureRepositoryLocalStorage configureRepositoryLocalStorage =
+    ({FutureFn<String>? baseDirFn, List<int>? encryptionKey, bool? clear}) {
   if (!kIsWeb) {
-    baseDirFn ??= () => getApplicationDocumentsDirectory().then((dir) => dir.path);
+    baseDirFn ??=
+        () => getApplicationDocumentsDirectory().then((dir) => dir.path);
   } else {
     baseDirFn ??= () => '';
   }
-  
+
   return hiveLocalStorageProvider
-    .overrideWithProvider(Provider((ref) => HiveLocalStorage(
+      .overrideWithProvider(Provider((ref) => HiveLocalStorage(
             hive: ref.read(hiveProvider),
             baseDirFn: baseDirFn,
             encryptionKey: encryptionKey,
@@ -32,41 +32,47 @@ final repositoryProviders = <String, Provider<Repository<DataModel>>>{
 };
 
 final repositoryInitializerProvider =
-  FutureProvider<RepositoryInitializer>((ref) async {
-    final adapters = <String, RemoteAdapter>{'layoutSettings': ref.watch(internalLayoutSettingsRemoteAdapterProvider)};
-    final remotes = <String, bool>{'layoutSettings': true};
+    FutureProvider<RepositoryInitializer>((ref) async {
+  final adapters = <String, RemoteAdapter>{
+    'layoutSettings': ref.watch(internalLayoutSettingsRemoteAdapterProvider)
+  };
+  final remotes = <String, bool>{'layoutSettings': true};
 
-    await ref.watch(graphNotifierProvider).initialize();
+  await ref.watch(graphNotifierProvider).initialize();
 
-    // initialize and register
-    for (final type in repositoryProviders.keys) {
-      final repository = ref.read(repositoryProviders[type]!);
-      repository.dispose();
-      await repository.initialize(
-        remote: remotes[type],
-        adapters: adapters,
-      );
-      internalRepositories[type] = repository;
-    }
+  // initialize and register
+  for (final type in repositoryProviders.keys) {
+    final repository = ref.read(repositoryProviders[type]!);
+    repository.dispose();
+    await repository.initialize(
+      remote: remotes[type],
+      adapters: adapters,
+    );
+    internalRepositories[type] = repository;
+  }
 
-    // deferred model initialization
+  // deferred model initialization
+  for (final repository in internalRepositories.values) {
+    await repository.remoteAdapter.internalInitializeModels();
+  }
+
+  ref.onDispose(() {
     for (final repository in internalRepositories.values) {
-      await repository.remoteAdapter.internalInitializeModels();
+      repository.dispose();
     }
+  });
 
-    ref.onDispose(() {
-      for (final repository in internalRepositories.values) {
-        repository.dispose();
-      }
-    });
-
-    return RepositoryInitializer();
+  return RepositoryInitializer();
 });
+
 extension RepositoryWidgetRefX on WidgetRef {
-  Repository<LayoutSetting> get layoutSettings => watch(layoutSettingsRepositoryProvider)..remoteAdapter.internalWatch = watch;
+  Repository<LayoutSetting> get layoutSettings =>
+      watch(layoutSettingsRepositoryProvider)
+        ..remoteAdapter.internalWatch = watch;
 }
 
 extension RepositoryRefX on Ref {
-
-  Repository<LayoutSetting> get layoutSettings => watch(layoutSettingsRepositoryProvider)..remoteAdapter.internalWatch = watch as Watcher;
+  Repository<LayoutSetting> get layoutSettings =>
+      watch(layoutSettingsRepositoryProvider)
+        ..remoteAdapter.internalWatch = watch as Watcher;
 }
--- lib/widget/layout/provider/layout_wrap.dart
+++ lib/widget/layout/provider/layout_wrap.dart
@@ -1,5 +1,8 @@
+import 'package:flex_demo/main.data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'layout_setting.dart';
 
 Widget defaultWidgetBuilder(BuildContext ctx) => const SizedBox();
 
@@ -19,4 +22,113 @@ class LayoutWarp {
 
 final layoutWarpProvider = Provider<LayoutWarp>((ref) {
   return LayoutWarp();
+});
+
+final bodyProvider = Provider.family<Widget, BuildContext>(
+    (ref, arg) => throw UnimplementedError());
+
+final layoutProvider = Provider.family<Widget, BuildContext>((ref, ctx) {
+  final model = ref.layoutSettings.watch(LayoutSetting());
+  final wrap = ref.watch(layoutWarpProvider);
+  Widget body = ref.watch(bodyProvider(ctx));
+  List<Widget> widgets = <Widget>[];
+
+  if (model.panelAlignment == PanelAlignment.center) {
+    body = Column(
+      children: [
+        body,
+        wrap.panel(ctx),
+      ],
+    );
+    widgets = [wrap.activity(ctx), wrap.primary(ctx), body, wrap.second(ctx)];
+    if (model.primaryPosition == PrimaryPosition.right) {
+      widgets = widgets.reversed.toList();
+    }
+    return Row(children: widgets);
+  }
+
+  if (model.panelAlignment == PanelAlignment.justify) {
+    widgets = [wrap.primary(ctx), body, wrap.second(ctx)];
+    if (model.primaryPosition == PrimaryPosition.right) {
+      widgets = widgets.reversed.toList();
+    }
+    widgets = [
+      wrap.activity(ctx),
+      Column(
+        children: [
+          Row(children: widgets),
+          wrap.panel(ctx),
+        ],
+      )
+    ];
+    if (model.primaryPosition == PrimaryPosition.right) {
+      widgets = widgets.reversed.toList();
+    }
+    return Row(children: widgets);
+  }
+
+  if (model.panelAlignment == PanelAlignment.left) {
+    if (model.primaryPosition == PrimaryPosition.left) {
+      return Row(children: [
+        wrap.activity(ctx),
+        Column(
+          children: [
+            Row(children: [
+              wrap.primary(ctx),
+              body,
+            ]),
+            wrap.panel(ctx),
+          ],
+        ),
+        wrap.second(ctx),
+      ]);
+    } else {
+      return Row(children: [
+        Column(
+          children: [
+            Row(children: [
+              wrap.second(ctx),
+              body,
+            ]),
+            wrap.panel(ctx),
+          ],
+        ),
+        wrap.primary(ctx),
+        wrap.activity(ctx),
+      ]);
+    }
+  }
+
+  if (model.panelAlignment == PanelAlignment.right) {
+    if (model.primaryPosition == PrimaryPosition.left) {
+      return Row(children: [
+        wrap.activity(ctx),
+        wrap.primary(ctx),
+        Column(
+          children: [
+            Row(children: [
+              body,
+              wrap.second(ctx),
+            ]),
+            wrap.panel(ctx),
+          ],
+        ),
+      ]);
+    } else {
+      return Row(children: [
+        wrap.second(ctx),
+        Column(
+          children: [
+            Row(children: [
+              body,
+              wrap.primary(ctx),
+            ]),
+            wrap.panel(ctx),
+          ],
+        ),
+        wrap.activity(ctx),
+      ]);
+    }
+  }
+  throw 'There is wrong enum state!';
 });
